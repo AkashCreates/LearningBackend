3ab55e2c9d6ba75a6d4969ed7184b22b204493f2
AkashCreates <akashmaityofficial@gmail.com>
2025-07-17 17:05:24 +0530

    js basics

---
+++ Day1/script.js
@@ -0,0 +1,80 @@
+//---------------------------------------- learning Array
+
+// console.log("hello");
+
+// let fruits = ["apple", "banana", "mango"];
+// let numbers = [1, 2, 3, 4, 5];
+// let mixed = ["Akash", 25, true];
+
+// fruits.unshift("kiwi");
+// console.log(fruits);
+
+//-------------------------------------- learning forEach
+
+// fruits.forEach((e, x, y) => {
+//   console.log(`${x}` + ": " + `${e}`);
+// });
+
+// let students = ["Akash", "Ravi", "Sneha"];
+
+// students.forEach((ele, index) => {
+//   console.log(`Student #${index + 1}: ${ele}`);
+// });
+
+// console.log(students);
+
+//  students.forEach((name, i) => {
+//   console.log("Student" + " " + "#"+ (i) + " "  + name);
+// });
+
+//------------------------------------------ learning map
+
+// const numbers = [1, 2, 3];
+// const square = numbers.forEach((num) => {
+//   return num * num;
+// });
+// console.log(square);
+
+// const numbers2 = [1, 2, 3];
+// const square2 = numbers2.map((num) => {
+// //   console.log(num * num);
+//   return num * num;
+// });
+// console.log(square2);
+
+// const numbers3 = [1, 2, 3];
+// const square3 = numbers3.map(i => i * i);
+// console.log(square3);
+
+// const arr = [23, 34, 100, 121];
+
+// const half = arr.map(i => i / 2);
+// console.log(half);
+
+//---------------------------------------- learnig filter
+
+// const arr = [23, 34, 100, 121];
+
+// const even = arr.filter(e => e % 2 === 0);
+// console.log(even);
+
+// --------------------------------learnig find---indexOf
+
+// const users = [
+//   { name: "Akash", id: 1 },
+//   { name: "Ravi", id: 2 },
+//   { name: "Sneha", id: 3 },
+// ];
+// // console.log(users);
+
+// // const demo = users.forEach((u) => {
+// //     console.log(u.name);
+// // });
+
+// let findingAperson = users.find(x => x.name === "Sneha");
+// console.log(findingAperson);
+
+
+// let fruits = ["apple", "banana", "mango", "grape"];
+// console.log(fruits.indexOf("Banana"));
+
